42beb48cdb3d5ef9ffbcd77f896239697fba17b3
FabiolaCop33 <fabiola.padilla@sacrecoeur.group>
2024-07-08 19:33:19 -0600

    Nuevo Archivo CentroBondadListViewdonde se tiene una lista de todos los centros de Bondad (centroBondad.name, centroBondad.adress)

---
+++ Generosity/View/CentroBondadListView.swift
@@ -0,0 +1,33 @@
+//
+//  CentroBondadListView.swift
+//  Generosity
+//
+//  Created by Familia Alvarado Perez on 08/07/24.
+//
+
+import SwiftUI
+
+struct CentroBondadListView: View {
+    @ObservedObject var viewModel: CentrosBondadViewModel
+
+    var body: some View {
+        List(viewModel.centrosBondad){centroBondad in
+            VStack(alignment: .leading){
+                Text(centroBondad.name)
+                    .font(.headline)
+                Text(centroBondad.address)
+                    .font(.subheadline)
+            }
+        }
+        .navigationTitle("Lista de Centros de Bondad")
+        .onAppear{
+            Task{
+                await viewModel.loadCentrosBondad()
+            }
+        }
+    }
+}
+
+#Preview {
+    CentroBondadListView()
+}
