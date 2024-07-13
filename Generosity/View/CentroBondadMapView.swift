4e383297ae9b198e59f20f003cdb24b1f064c3b8
FabiolaCop33 <fabiola.padilla@sacrecoeur.group>
2024-07-12 18:51:48 -0600

    Se creo la nueva variable selectedStateId en CentroBondadMapView para aplicar el filtro por estado que se va a implementar mas adelnte, se ajustaron algunos espacios y se agrego la condicion de selectedStateID en onAppear

--- Generosity/View/CentroBondadMapView.swift
+++ Generosity/View/CentroBondadMapView.swift
@@ -13,25 +13,30 @@ struct CentroBondadMapView: View {
     @State private var showModal: Bool = false
     @State private var navigateToList: Bool = false
     let viewModel = CentrosBondadViewModel()
+    @State private var selectedStateID: Int? = nil
+    
     
     var body: some View {
-            Map() {
-                ForEach(viewModel.centrosBondad) { centroBondad in
-                    Annotation(centroBondad.name, coordinate: centroBondad.coordinate){
-                        Image(systemName: "house.circle")
-                            .shadow(color:.pink, radius: 3)
-                            .onTapGesture {
-                                selectedCentroBondad = centroBondad
-                            }
-                    }
+        Map() {
+            ForEach(viewModel.centrosBondad) { centroBondad in
+                Annotation(centroBondad.name, coordinate: centroBondad.coordinate){
+                    Image(systemName: "house.circle")
+                        .shadow(color:.pink, radius: 3)
+                        .onTapGesture {
+                            selectedCentroBondad = centroBondad
+                        }
                 }
             }
-        .onAppear{
-                Task {
-                await viewModel.loadCentrosBondad()
-                }
         }
-        .sheet(item: $selectedCentroBondad) { centroBondand in
+        .onAppear {
+            Task {
+                await viewModel.loadCentrosBondad()
+                // Optionally filter based on selectedStateID if it has a value
+                if let stateID = selectedStateID {
+                    viewModel.centrosBondad = viewModel.filterCentrosById(centros: viewModel.centrosBondad, id: stateID)
+                }
+            }
+        }.sheet(item: $selectedCentroBondad) { centroBondand in
             CentroBondadDetailView(centroBondad: centroBondand)
         }
     }
